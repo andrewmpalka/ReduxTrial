ac66007318080627c83079bba74224bbabf0c350
Andrew Palka <apalka@coolfiresolutions.com>
2018-02-26 13:34:32 -0600

    Initial Commit

---
+++ ReduxTrial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReduxTrial
+//
+//  Created by Andrew Palka on 2/26/18.
+//  Copyright © 2018 Andy Palka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
